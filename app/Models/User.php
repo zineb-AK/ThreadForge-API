9a652b1ffb47e8740760199e738548909109b702
zineb19971231 <zinebakarras@gmail.com>
2026-06-23 15:38:25 +0100

    add bluprints relationship to User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -30,4 +30,9 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+
+    public function bluprints(){
+        return $this ->hasMany(Blueprint::class);
+    }
 }
---
+++ database/migrations/2026_06_23_111022_create_bleuprints_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('bleuprints', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('tone');
+            $table->string('target_audience');
+            $table->integer('max_characters');
+            $table->integer('max_hashtags');
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('bleuprints');
+    }
+};
