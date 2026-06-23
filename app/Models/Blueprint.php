b68ada388257df6e60562ec812a00c05b517cdfc
zineb19971231 <zinebakarras@gmail.com>
2026-06-23 15:58:12 +0100

    modifier controller bluerprint

---
+++ app/Models/Blueprint.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Blueprint extends Model
+{
+    protected $fillable=
+    ['name','tone','target_audience','max_characters','max_hashtags','user_id'];
+
+    public function user(){
+        return $this ->belongsTo(User::class);
+    }
+}
